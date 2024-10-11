84035c2ae5bbc5dfc533d3493f397b73794fd7d0
eduardonk9999 <thiago.novak7@gmail.com>
2024-10-11 09:40:37 -0300

    feat: config projeto

---
+++ src/main/java/com/eventdriven/orders/EventdrivenApplication.java
@@ -0,0 +1,13 @@
+package com.eventdriven.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventdrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventdrivenApplication.class, args);
+	}
+
+}
